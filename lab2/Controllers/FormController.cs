8c453d7140c5bebe5afb7b0ff067ae0ed6555cef
poborowski <bpoborowski@gmail.com>
2021-12-30 19:05:59 +0100

    s
    s

---
+++ lab2/Controllers/FormController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab2_test.Models;
+
+namespace lab2_test.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Created", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ lab2/Models/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab2_test.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imie")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Data")]
+        public DateTime Description { get; set; }
+      
+    }
+}
